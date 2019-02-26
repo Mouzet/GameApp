1043187fab9dc7439b2234cc1eaab811bc14358a
Mouzet <mermod.yannick@gmail.com>
2019-02-26 21:01:46 +0100

    First try for the layout of the front page

--- app/src/main/java/com/example/gameapp/MainActivity.java
+++ app/src/main/java/com/example/gameapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.gameapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +12,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int spu;
+        // Coding the first spinner (genre)
+        Spinner spinner1 = (Spinner) findViewById(R.id.genre);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                R.array.genre, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner1.setAdapter(adapter1);
+
+        //Coding the second spinner (nb_etoile)
+        Spinner spinner2 = (Spinner) findViewById(R.id.nb_etoile);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.nb_etoile, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner2.setAdapter(adapter2);
+
+
+
     }
 }
 
