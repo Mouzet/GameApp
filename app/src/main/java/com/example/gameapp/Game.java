a81de2ab4fbc4830dac4e322bf3fa6affc871e66
Mouzet <mermod.yannick@gmail.com>
2019-03-02 00:34:50 +0100

    Testing the intent of the button Validate

---
+++ app/src/main/java/com/example/gameapp/Game.java
@@ -0,0 +1,28 @@
+package com.example.gameapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class Game extends AppCompatActivity{
+
+    private String name;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.games);
+
+        Intent intent = getIntent();
+        name = intent.getStringExtra("name");
+
+        TextView name_game = (TextView) findViewById(R.id.name_game1);
+        name_game.setText(name);
+
+    }}
--- app/src/main/java/com/example/gameapp/MainActivity.java
+++ app/src/main/java/com/example/gameapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.gameapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,15 +25,13 @@ public class MainActivity extends AppCompatActivity {
 
         spinner1.setAdapter(adapter1);
 
-        //Coding the second6 spinner (nb_etoile)
+        //Coding the second spinner (nb_etoile)
         Spinner spinner2 = (Spinner) findViewById(R.id.nb_etoile);
         ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
                 R.array.nb_etoile, android.R.layout.simple_spinner_item);
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spinner2.setAdapter(adapter2);
-
-        // creating the intent for the navbar ?
 
 
 
@@ -42,5 +44,16 @@ public class MainActivity extends AppCompatActivity {
         return true;
 
     }
+
+    // creating the intent for the validate button
+
+    public void validateSearch(View view){
+        Intent intent = new Intent(this, Game.class);
+        EditText name = (EditText)findViewById(R.id.name_typed);
+        intent.putExtra("name", name.getText().toString());
+
+        startActivity(intent);
+    }
+
 }
 
